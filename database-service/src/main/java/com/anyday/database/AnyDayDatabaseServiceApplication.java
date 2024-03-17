20c8360e22a4e91c663ef4016626fbcd46f2aa43
Jin Hyoung Joo <hyoungjoo.j@gmail.com>
2024-03-17 22:18:52 +0900

    feat: add initial database service code

---
+++ database-service/src/main/java/com/anyday/database/AnyDayDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.anyday.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyDayDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyDayDatabaseServiceApplication.class, args);
+	}
+
+}
---
+++ database-service/src/test/java/com/anyday/database/AnyDayDatabaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anyday.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyDayDatabaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
